4c92080aa9961a29d01736d8f0ccf20026153f06
Салихов Рашит <salihov-rashit@yandex.ru>
2021-05-02 13:28:27 +0500

    Update class LogInReg

--- src/main/java/Package/LogInReg.java
+++ src/main/java/Package/LogInReg.java
@@ -69,6 +69,8 @@ public class LogInReg extends HttpServlet {
             Connection con =null;
             Statement stmt=null;
             ResultSet rs=null;
+            ResultSet rs1=null;
+            String arr[] ={"length","width","height","floor","otdelka"};
             if (page.equals("LogIn")) {
                 try {
                     con = getConnection();
@@ -84,6 +86,20 @@ public class LogInReg extends HttpServlet {
 
                     if (rs.next()){
                         go=1;
+                        String query1;
+
+                        for(int i =0;i<5;i++){
+                            query1 = "SELECT "+arr[i]+" FROM settings WHERE type='winter';";
+                            rs1 = stmt.executeQuery(query1);
+                            if (rs1.next()){
+                            request.setAttribute("summer"+i,rs1.getString(arr[i]));
+                            }
+                            query1 = "SELECT "+arr[i]+" FROM settings WHERE type='summer';";
+                            rs1 = stmt.executeQuery(query1);
+                            if (rs1.next()){
+                                request.setAttribute("winter"+i,rs1.getString(arr[i]));
+                            }
+                        }
                     }
                     else{
                         request.setAttribute("result", "Логин/Пароль не верен");
@@ -91,11 +107,11 @@ public class LogInReg extends HttpServlet {
                     }
 
                 }catch (NullPointerException sqlNPE){
-                    request.setAttribute("result","Error: "+sqlNPE.getMessage());
+                    request.setAttribute("result","Error1: "+sqlNPE.getLocalizedMessage());
                     go = 0;
                 }
                 catch (SQLException sqlEx) {
-                    request.setAttribute("result","Error: "+sqlEx.getSQLState());
+                    request.setAttribute("result","Error2: "+sqlEx.getSQLState());
                     go = 0;
                 }
                 finally {
@@ -169,122 +185,3 @@ public class LogInReg extends HttpServlet {
 
 
 }
-
-
-
-/*public class LogInReg {
-
-    void logWin() {
-        JFrame LogInWindow = new JFrame("Вход");
-        LogInWindow.setTitle("Вход");
-        LogInWindow.setBounds(100, 100, 266, 290);
-        LogInWindow.setResizable(false);
-
-        JPanel panel = new JPanel();
-
-        JLabel logl = new JLabel("Логин:");
-        logl.setBounds(40,10,140,30);
-        JLabel pasl = new JLabel("Пароль:");
-        pasl.setBounds(40,70,140,30);
-        JLabel info = new JLabel();
-        info.setBounds(10, 217,250,30);
-
-        JButton enter = new JButton("Войти");
-        JButton reg = new JButton("Регистрация");
-        JButton unhide = new JButton();
-
-        JTextField log = new JTextField();
-        log.setBounds(40,40,140,30);
-        JPasswordField pas = new JPasswordField();
-        pas.setBounds(40,100,140,30);
-
-        enter.setBounds(50,135,120,40);
-        reg.setBounds(50,176,120,40);
-        unhide.setBounds(181,102,25,25);
-
-        reg.addActionListener(e -> {
-            info.setText("");
-            if (reg.getText().equals("Регистрация")){
-                reg.setText("Назад");
-                LogInWindow.setTitle("Регистрация");
-                enter.setText("Регистрация");
-            }
-            else{
-                reg.setText("Регистрация");
-                LogInWindow.setTitle("Вход");
-                enter.setText("Войти");
-            }
-        });
-
-        enter.addActionListener(e -> {
-            info.setText("");
-            if (enter.getText().equals("Войти")){
-                if (log.getText().equals("admin")&&(Arrays.equals(pas.getPassword(),new char[]{'a','d','m','i','n'}))){ //аккаунт администратора
-                    Admin adm = new Admin();
-                    adm.logWin();
-                    Calculator calc = new Calculator();
-                    calc.calculate();
-                    LogInWindow.dispose();
-                }
-                else{
-                    //Проверка логина-пароля
-                    try{
-                        File file = new File("./src/main/resources","File.txt");
-                        File fjar = new File("..","File.txt");
-                        if (file.exists()||fjar.exists()){
-                        FileInputStream input = new FileInputStream(file);
-
-                        Calculator calc = new Calculator();
-                        calc.calculate();
-                        LogInWindow.dispose();
-                    }
-                    else{
-                        file.createNewFile();
-                        info.setText("Данного пользователя не существует");
-                    }
-                    }
-                    catch (IOException a){System.out.print(a);}
-
-                }
-            }
-            else{
-                //Регистрация нового пользователя
-           }
-        });
-
-        unhide.addMouseListener(new MouseListener() {
-
-            @Override
-            public void mousePressed(MouseEvent e) {
-                pas.setEchoChar((char)0);
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                pas.setEchoChar('•');
-            }
-
-            @Override
-            public void mouseClicked(MouseEvent e) {}
-            @Override
-            public void mouseEntered(MouseEvent e) {}
-            @Override
-            public void mouseExited(MouseEvent e) {}
-        });
-
-        panel.add(logl);
-        panel.add(pasl);
-        panel.add(log);
-        panel.add(pas);
-        panel.add(unhide);
-        panel.add(enter);
-        panel.add(reg);
-        panel.add(info);
-        panel.setLayout(null);
-        LogInWindow.add(panel);
-
-        LogInWindow.setVisible(true);
-        LogInWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-    }
-}*/
